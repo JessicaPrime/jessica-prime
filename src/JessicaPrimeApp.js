997ba7f1251a66eac63ea8bd716da1c4b5c1e980
Jéssica Soares <sodijessi@gmail.com>
2025-05-17 13:02:00 -0300

    Adiciona componente default JessicaPrimeApp

--- src/JessicaPrimeApp.js
+++ src/JessicaPrimeApp.js
@@ -5,3 +5,23 @@ import { Textarea } from "./components/ui/Textarea";
 import { TabsList, TabsTrigger } from "./components/ui/Tabs";
 import { Switch } from "./components/ui/Switch";
 import { Checkbox } from "./components/ui/Checkbox";
+export default function JessicaPrimeApp() {
+  return (
+    <div style={{ padding: 20 }}>
+      <h1>Jessica Prime App</h1>
+      <p>Bem-vinda ao seu aplicativo React!</p>
+      <Button>Exemplo de Botão</Button>
+      <Card>
+        <CardContent>Conteúdo do Card</CardContent>
+      </Card>
+      <Input placeholder="Digite algo..." />
+      <Textarea placeholder="Mensagem" />
+      <TabsList>
+        <TabsTrigger value="tab1">Aba 1</TabsTrigger>
+        <TabsTrigger value="tab2">Aba 2</TabsTrigger>
+      </TabsList>
+      <Switch />
+      <Checkbox />
+    </div>
+  );
+}
